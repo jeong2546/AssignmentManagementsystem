8f575d7ee6a764f1f3a33d25a6056b9ed00d76ff
82102 <82102@115.160.126.46>
2020-03-20 22:11:48 +0900

    modify some menu

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,12 +1,12 @@
 public class Assignment {
 	public static void main(String[] args) {
 		System.out.println("***assignmentmanagementsystem***");
-		System.out.println("Add assignment :");
-		System.out.println("Delete assignment :");
-		System.out.println("Edit assignment :");
-		System.out.println("view assignment :");
-		System.out.println("view last semester assignment :");
-		System.out.println("exit ");
+		System.out.println("1. Add assignment ");
+		System.out.println("2. Delete assignment ");
+		System.out.println("3. Edit assignment ");
+		System.out.println("4. view assignment ");
+		System.out.println("5. view last semester assignment ");
+		System.out.println("6. exit ");
 
 		
 	}
